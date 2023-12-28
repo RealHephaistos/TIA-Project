c47b67c7408f07b66205279f41b7b151e120d4cb
Berrouche Issameddine <issameddine.berrouche@etudiant.univ-rennes1.fr>
2023-12-28 21:24:21 +0100

    Add terrain, player and player rigidbody behavior

---
+++ Assets/Scripts/TerrainObserverEventHandler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TerrainObserverEventHandler : DefaultObserverEventHandler
+{
+    private GameObject terrain;
+    private GameObject player;
+    private Rigidbody playerRigidbody;
+
+    protected virtual void Start()
+    {
+        base.Start();
+
+        terrain = GameObject.Find("Terrain");
+        player = GameObject.Find("Player");
+        playerRigidbody = player.GetComponent<Rigidbody>();
+    }
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+
+        // Set player rigidbody IsKinematic to false
+        playerRigidbody.isKinematic = false;
+    }
+}
