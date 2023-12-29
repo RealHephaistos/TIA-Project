6c4311ae388be2f36a9712409f9affd2d6ac5868
Berrouche Issameddine <issameddine.berrouche@etudiant.univ-rennes1.fr>
2023-12-29 22:48:46 +0100

    Imporved golf ball model and player controller

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -8,19 +8,29 @@ public class PlayerControls : MonoBehaviour
 {
     [SerializeField] private GameObject leanJoystick;
     private LeanJoystick leanJoystickScript;
-    [SerializeField] private float moveSpeed = 5f;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float friction;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         leanJoystickScript = leanJoystick.GetComponent<LeanJoystick>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveVector = leanJoystickScript.ScaledValue;
-        Vector3 moveDirection = new Vector3(moveVector.x, 0, moveVector.y);
-        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+        // Add force to the player in the direction of the joystick
+        Vector3 movement = new Vector3(leanJoystickScript.ScaledValue.x, 0, leanJoystickScript.ScaledValue.y);
+        // Ajust the movement vector relative to the camera
+        movement = Camera.main.transform.TransformDirection(movement);
+        rb.AddForce(movement * moveSpeed);
+
+        // Apply friction to the player
+        rb.velocity *= friction;
+        rb.angularVelocity *= friction;
     }
 }
